7404fbc0959d24ef0f98ebd63e6867151d70af48
David E. Mevorah <demev7488@hotmail.com>
2018-07-18 15:51:33 -0700

    reset counter, array clear, special character variable change

--- HyphyOregonConferences/Program.cs
+++ HyphyOregonConferences/Program.cs
@@ -74,190 +74,276 @@ namespace HyphyOregonConferences
         private static string[] ownerNames()
         {
             //Special Character Check variable
-            Regex r = new Regex(@"[~`!@#$%^&*()+=|\{}':;.,<>/?[\]""_-]");
+            Regex specialCharacters = new Regex(@"[~`!@#$%^&*()-+=|\{}':;.,<>/?]");
+
 
             //Array to read the loop of inputs the user enters
             string[] ownerNameInput = new string[10];
+            //Array Counter
+            int resetCounter = ownerNameInput.Length;
+
+
             ownerNameInput[0] = Console.ReadLine();
             if (int.TryParse(ownerNameInput[0], out int validationResult))
             {
                 Console.WriteLine("You entered a number. Please enter an owner's name in character values only.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 10;
             }
             else if (ownerNameInput[0].Length == 0)
             {
                 Console.WriteLine("You did not enter an owner's name. Please enter in an owner's name.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 10;
             }
             else if (ownerNameInput[0].Length > 15)
             {
                 Console.WriteLine("You entered an owner's name that is over 15 characters. Please only enter in an owner's first name.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 10;
             }
-            else if(r.IsMatch(ownerNameInput[0]))
+            else if (specialCharacters.IsMatch(ownerNameInput[0]))
             {
                 Console.WriteLine("You entered a special character. Please enter an owner's name in character values only.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 10;
             }
-
 
             ownerNameInput[1] = Console.ReadLine();
             if (int.TryParse(ownerNameInput[1], out int validationResult1))
             {
                 Console.WriteLine("You entered a number. Please enter an owner's name in character values only.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 9;
             }
             else if (ownerNameInput[1].Length == 0)
             {
                 Console.WriteLine("You did not enter an owner's name. Please enter in an owner's name.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 9;
             }
             else if (ownerNameInput[1].Length > 15)
             {
                 Console.WriteLine("You entered an owner's name that is over 15 characters. Please only enter in an owner's first name.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 9;
             }
-            else if (r.IsMatch(ownerNameInput[1]))
+            else if (specialCharacters.IsMatch(ownerNameInput[1]))
             {
                 Console.WriteLine("You entered a special character. Please enter an owner's name in character values only.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 9;
             }
-
 
             ownerNameInput[2] = Console.ReadLine();
             if (int.TryParse(ownerNameInput[2], out int validationResult2))
             {
                 Console.WriteLine("You entered a number. Please enter an owner's name in character values only.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 8;
             }
             else if (ownerNameInput[2].Length == 0)
             {
                 Console.WriteLine("You did not enter an owner's name. Please enter in an owner's name.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 8;
             }
             else if (ownerNameInput[2].Length > 15)
             {
                 Console.WriteLine("You entered an owner's name that is over 15 characters. Please only enter in an owner's first name.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 8;
             }
-            else if (r.IsMatch(ownerNameInput[2]))
+            else if (specialCharacters.IsMatch(ownerNameInput[2]))
             {
                 Console.WriteLine("You entered a special character. Please enter an owner's name in character values only.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 8;
             }
 
             ownerNameInput[3] = Console.ReadLine();
             if (int.TryParse(ownerNameInput[3], out int validationResult3))
             {
                 Console.WriteLine("You entered a number. Please enter an owner's name in character values only.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 7;
             }
             else if (ownerNameInput[3].Length == 0)
             {
                 Console.WriteLine("You did not enter an owner's name. Please enter in an owner's name.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 7;
             }
             else if (ownerNameInput[3].Length > 15)
             {
                 Console.WriteLine("You entered an owner's name that is over 15 characters. Please only enter in an owner's first name.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 7;
             }
-            else if (r.IsMatch(ownerNameInput[3]))
+            else if (specialCharacters.IsMatch(ownerNameInput[3]))
             {
                 Console.WriteLine("You entered a special character. Please enter an owner's name in character values only.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 7;
             }
+
 
             ownerNameInput[4] = Console.ReadLine();
             if (int.TryParse(ownerNameInput[4], out int validationResult4))
             {
                 Console.WriteLine("You entered a number. Please enter an owner's name in character values only.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 6;
             }
             else if (ownerNameInput[4].Length == 0)
             {
                 Console.WriteLine("You did not enter an owner's name. Please enter in an owner's name.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 6;
             }
             else if (ownerNameInput[4].Length > 15)
             {
                 Console.WriteLine("You entered an owner's name that is over 15 characters. Please only enter in an owner's first name.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 6;
             }
-            else if (r.IsMatch(ownerNameInput[4]))
+            else if (specialCharacters.IsMatch(ownerNameInput[4]))
             {
                 Console.WriteLine("You entered a special character. Please enter an owner's name in character values only.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 6;
             }
 
             ownerNameInput[5] = Console.ReadLine();
             if (int.TryParse(ownerNameInput[5], out int validationResult5))
             {
                 Console.WriteLine("You entered a number. Please enter an owner's name in character values only.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 5;
             }
             else if (ownerNameInput[5].Length == 0)
             {
                 Console.WriteLine("You did not enter an owner's name. Please enter in an owner's name.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 5;
             }
             else if (ownerNameInput[5].Length > 15)
             {
                 Console.WriteLine("You entered an owner's name that is over 15 characters. Please only enter in an owner's first name.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 5;
             }
-            else if (r.IsMatch(ownerNameInput[5]))
+            else if (specialCharacters.IsMatch(ownerNameInput[5]))
             {
                 Console.WriteLine("You entered a special character. Please enter an owner's name in character values only.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 5;
             }
+
 
             ownerNameInput[6] = Console.ReadLine();
             if (int.TryParse(ownerNameInput[6], out int validationResult6))
             {
                 Console.WriteLine("You entered a number. Please enter an owner's name in character values only.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 4;
             }
             else if (ownerNameInput[6].Length == 0)
             {
                 Console.WriteLine("You did not enter an owner's name. Please enter in an owner's name.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 4;
             }
             else if (ownerNameInput[6].Length > 15)
             {
                 Console.WriteLine("You entered an owner's name that is over 15 characters. Please only enter in an owner's first name.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 4;
             }
-            else if (r.IsMatch(ownerNameInput[6]))
+            else if (specialCharacters.IsMatch(ownerNameInput[6]))
             {
                 Console.WriteLine("You entered a special character. Please enter an owner's name in character values only.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 4;
             }
 
             ownerNameInput[7] = Console.ReadLine();
             if (int.TryParse(ownerNameInput[7], out int validationResult7))
             {
                 Console.WriteLine("You entered a number. Please enter an owner's name in character values only.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 3;
             }
             else if (ownerNameInput[7].Length == 0)
             {
                 Console.WriteLine("You did not enter an owner's name. Please enter in an owner's name.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 3;
             }
             else if (ownerNameInput[7].Length > 15)
             {
                 Console.WriteLine("You entered an owner's name that is over 15 characters. Please only enter in an owner's first name.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 3;
             }
-            else if (r.IsMatch(ownerNameInput[7]))
+            else if (specialCharacters.IsMatch(ownerNameInput[7]))
             {
                 Console.WriteLine("You entered a special character. Please enter an owner's name in character values only.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 3;
             }
+
 
             ownerNameInput[8] = Console.ReadLine();
             if (int.TryParse(ownerNameInput[8], out int validationResult8))
             {
                 Console.WriteLine("You entered a number. Please enter an owner's name in character values only.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 2;
             }
             else if (ownerNameInput[8].Length == 0)
             {
                 Console.WriteLine("You did not enter an owner's name. Please enter in an owner's name.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 2;
             }
             else if (ownerNameInput[8].Length > 15)
             {
                 Console.WriteLine("You entered an owner's name that is over 15 characters. Please only enter in an owner's first name.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 2;
             }
-            else if (r.IsMatch(ownerNameInput[8]))
+            else if (specialCharacters.IsMatch(ownerNameInput[8]))
             {
                 Console.WriteLine("You entered a special character. Please enter an owner's name in character values only.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 2;
             }
 
             ownerNameInput[9] = Console.ReadLine();
             if (int.TryParse(ownerNameInput[9], out int validationResult9))
             {
                 Console.WriteLine("You entered a number. Please enter an owner's name in character values only.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 1;
             }
             else if (ownerNameInput[9].Length == 0)
             {
                 Console.WriteLine("You did not enter an owner's name. Please enter in an owner's name.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 1;
             }
             else if (ownerNameInput[9].Length > 15)
             {
                 Console.WriteLine("You entered an owner's name that is over 15 characters. Please only enter in an owner's first name.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 1;
             }
-            else if (r.IsMatch(ownerNameInput[9]))
+            else if (specialCharacters.IsMatch(ownerNameInput[9]))
             {
                 Console.WriteLine("You entered a special character. Please enter an owner's name in character values only.");
+                Array.Clear(ownerNameInput, 0, ownerNameInput.Length);
+                resetCounter = 1;
             }
 
             return ownerNameInput;
